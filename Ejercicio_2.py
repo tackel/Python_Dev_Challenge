9944be2a8b5cb4271279c9a55b766b64244ee624
Fernando Verri <74513344+tackel@users.noreply.github.com>
2022-11-02 00:58:10 -0300

    ejercicio 2 modificado

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -1,14 +1,44 @@
 import pandas as pd
 
-
 _PRODUCT_DF = pd.DataFrame({
-    "product_name": ["Chocolate", "Granizado","Limon", "Dulce de Leche"], 
-    "quantity": [3,10,0,5]
-    })
+    "product_name": ["Chocolate", "Granizado", "Limon", "Dulce de Leche"],
+    "quantity": [3, 10, 0, 5]
+})
+cont = 0
 
 
 def is_product_available(product_name, quantity):
-    print(_PRODUCT_DF.loc[_PRODUCT_DF['product_name']==product_name]['quantity'])
-    
+    global cont
+    # Compruebo si el producto ingresado existe en la columna product_name
+    product_exist = _PRODUCT_DF['product_name'].isin([product_name]).any()
+    if product_exist == False:
+        cont += 1
+        if cont > 5:
+            return print('Ha ingresado valores incorrectos muchas veces.')
+        return False
 
-is_product_available('Chocolate',3)
+    # Busca el stock del producto ingresado
+    stock = _PRODUCT_DF.loc[_PRODUCT_DF['product_name']
+                            == product_name]['quantity']
+    response = True if int(quantity) <= int(stock) else False
+    if response == False:
+        cont += 1
+    else:
+        cont = 0
+    if cont > 5:
+        return print('Ha ingresado valores incorrectos muchas veces.')
+    return response
+
+
+# ejemplos de pedidos
+print(is_product_available('Granizad', 2))
+print(is_product_available('Granizad', 2))
+print(is_product_available('Granizad', 2))
+print(is_product_available('Granizad', 2))
+print(is_product_available('Granizad', 2))
+print(is_product_available('Granizad', 2))
+print(is_product_available('Granizad', 2))
+print(is_product_available('Granizad', 2))
+print(is_product_available('Granizad', 2))
+print(is_product_available('Granizad', 2))
+print(is_product_available('Granizad', 2))
